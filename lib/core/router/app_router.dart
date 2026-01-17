8549c16dfd40dfa125de29993a5aa1c278dd1159
App Developer <mobiledeveloper@perfecthandssolutions.com>
2026-01-18 04:04:57 +0530

    feat: add routes for attendance, fees, and class details views in NavigationService

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:school_management_system/features/app_features/views/features_page.dart';
+import 'package:school_management_system/features/attendance/views/attendance_view.dart';
 import 'package:school_management_system/features/auth/views/forget_pass_page.dart';
 import 'package:school_management_system/features/chat/views/chat_page.dart';
+import 'package:school_management_system/features/class/views/class_details_view/class_details_view.dart';
+import 'package:school_management_system/features/class/views/class_view/class_view.dart';
 import 'package:school_management_system/features/dashboard/views/dashboard_page.dart';
+import 'package:school_management_system/features/employees/views/employees_view.dart';
+import 'package:school_management_system/features/fees/views/fees_view/fees_view.dart';
+import 'package:school_management_system/features/guardians/view/guardian_view.dart';
 import 'package:school_management_system/features/profile/views/profile_page.dart';
+import 'package:school_management_system/features/students/views/students_view.dart';
 
 import '../../features/auth/views/splash_page.dart';
 import 'nave_bar_page.dart';
@@ -34,6 +41,37 @@ class NavigationService {
         GoRoute(
           path: Routes.forgotPassword,
           builder: (context, state) => const ForgetPassPage(),
+        ),
+        GoRoute(
+          path: Routes.employees,
+          builder: (context, state) => const EmployeesView(),
+        ),
+        GoRoute(
+          path: Routes.students,
+          builder: (context, state) => const StudentsView(),
+        ),
+        GoRoute(
+          path: Routes.classes,
+          builder: (context, state) => const ClassView(),
+        ),
+        GoRoute(
+          path: Routes.classDetail,
+          builder: (context, state) {
+            final classId = state.extra as String?;
+            return ClassDetailsView(classId: classId);
+          },
+        ),
+        GoRoute(
+          path: Routes.guardians,
+          builder: (context, state) => const GuardianView(),
+        ),
+        GoRoute(
+          path: Routes.attendance,
+          builder: (context, state) => const AttendanceView(),
+        ),
+        GoRoute(
+          path: Routes.fees,
+          builder: (context, state) => const FeesView(),
         ),
 
         /// Bottom Navigation Bar with nested routes
