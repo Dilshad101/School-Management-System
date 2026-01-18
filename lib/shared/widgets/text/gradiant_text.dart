bb6492eb21a34414e855210d16f3e0828643879d
App Developer <mobiledeveloper@perfecthandssolutions.com>
2026-01-18 23:57:37 +0530

    fix: make colors parameter optional in GradientText widget and provide default gradient

--- lib/shared/widgets/text/gradiant_text.dart
+++ lib/shared/widgets/text/gradiant_text.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:school_management_system/shared/styles/app_colors.dart';
 
 class GradientText extends StatelessWidget {
-  const GradientText(this.data, {super.key, required this.colors, this.style});
+  const GradientText(this.data, {super.key, this.colors, this.style});
 
   final String data;
-  final List<Color> colors;
+  final List<Color>? colors;
   final TextStyle? style;
 
   @override
   Widget build(BuildContext context) {
     return ShaderMask(
       shaderCallback: (bounds) => LinearGradient(
-        colors: colors,
+        colors: colors ?? [AppColors.secondary, AppColors.primary],
         tileMode: TileMode.mirror,
       ).createShader(bounds),
       child: Text(
