75a6422eb3c619390d32b2b7ea9cfc3350fda40b
App Developer <mobiledeveloper@perfecthandssolutions.com>
2026-02-19 16:44:16 +0530

    chore: remove unused imports and commented-out code from ProfilePage

--- lib/features/profile/views/profile_page.dart
+++ lib/features/profile/views/profile_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:school_management_system/core/router/route_paths.dart';
-import 'package:school_management_system/core/storage/local_storage.dart';
-import 'package:school_management_system/core/utils/di.dart';
 import 'package:school_management_system/features/auth/blocs/auth/auth_bloc.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -25,6 +23,3 @@ class ProfilePage extends StatelessWidget {
     );
   }
 }
-
-//https://api.dev.waad.co.in/api/v1/student-users/?page=1&pagesize=10&search=
-//https://api.dev.waad.co.in/api/v1/student-users/?page=1&pagesize=10
